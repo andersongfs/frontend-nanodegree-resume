d0af4993162a7c1149a9b23da48a5601f9120dcc
Anderson Gustafson <andersongfs@gmail.com>
2016-07-21 16:48:20 -0300

    part of Lesson 1 - dada types

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,16 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+//--1
+var awesomeThoughts = "I am Anderson and I am AWESOME!"
+funThoughts = awesomeThoughts.replace("AWESOME", "FUN")
+console.log(funThoughts);
+
+var name = "Anderson Gustafson";
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = "Software Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedName);
+$("#header").append(formattedRole);
